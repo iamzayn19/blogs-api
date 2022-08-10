0d6a99e0de65ab1b77333c56da251daa6b27c993
zain <iamzayn19@gmail.com>
2022-08-10 16:46:37 +0500

    create blog likes migration

---
+++ app/models/blog_like.rb
@@ -0,0 +1,4 @@
+class BlogLike < ApplicationRecord
+  belongs_to :user
+  belongs_to :blog
+end
---
+++ db/migrate/20220810114605_create_blog_likes.rb
@@ -0,0 +1,10 @@
+class CreateBlogLikes < ActiveRecord::Migration[6.1]
+  def change
+    create_table :blog_likes do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :blog, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
