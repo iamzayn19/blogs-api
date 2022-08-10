12a5db3983a02819aa5167b3e54c4b7fae31fbb1
zain <iamzayn19@gmail.com>
2022-08-10 16:47:01 +0500

    add post like table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_10_112818) do
+ActiveRecord::Schema.define(version: 2022_08_10_114605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,15 @@ ActiveRecord::Schema.define(version: 2022_08_10_112818) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "blog_likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "blog_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["blog_id"], name: "index_blog_likes_on_blog_id"
+    t.index ["user_id"], name: "index_blog_likes_on_user_id"
+  end
+
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -65,5 +74,7 @@ ActiveRecord::Schema.define(version: 2022_08_10_112818) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "blog_likes", "blogs"
+  add_foreign_key "blog_likes", "users"
   add_foreign_key "blogs", "users"
 end
