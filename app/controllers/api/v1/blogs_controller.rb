8e3b4853039c85b2e64a6d11d9d5c9594992e839
zain <iamzayn19@gmail.com>
2022-08-12 10:34:13 +0500

    shaping the blogs controller

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -1,9 +1,25 @@
 module Api
   module V1
     class BlogsController < ApiController
-
+      skip_before_action :authenticate_request, only: [:index, :show]
+      before_action :set_blog, only: [:show, :destroy]
+      
       def index
-        @blogs = Blog.all
+        render json: @blogs, status: :ok
+      end
+      
+      def show
+        render json: @blog, status: :ok
+      end
+      
+      def destroy
+        render json: "Destroy called", status: :ok
+      end
+      
+      private
+
+      def set_blog
+        @blog = Blog.find(params[:id])
       end 
 
     end
