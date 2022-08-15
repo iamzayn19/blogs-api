6cff9c1e42317f25d581abec8310e28fc028ddbd
zain <iamzayn19@gmail.com>
2022-08-15 09:48:44 +0500

    Create blog functionality

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -9,6 +9,20 @@ module Api
         render json: @blogs
       end
       
+      def create
+        if @current_user.user_type == "public"
+          render json: "You are not allowed to create a blog"
+        else
+          params[:user_id] = @current_user.id
+          @blog = Blog.create(blog_params)
+          if @blog.save
+            render json: "Blog created successfully. Please wait for the admin to publish it"
+          else
+            render json: "Blog creation unsuccessful! Please try again."
+          end
+        end  
+      end 
+
       def show
         render json: @blog, status: :ok
       end
@@ -18,6 +32,10 @@ module Api
       end
       
       private
+
+      def blog_params
+        params.permit(:title,:body,:user_id,images:[])
+      end 
 
       def set_blog
         @blog = Blog.find(params[:id])
