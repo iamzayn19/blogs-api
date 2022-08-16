a128cf9b2216bbc3fffd4844e58b98a9c8fc96b0
zain <iamzayn19@gmail.com>
2022-08-16 12:45:19 +0500

    Add active admin comments

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -1,16 +1,16 @@
-ActiveAdmin.register Blog do
+ActiveAdmin.register Comment, :as => "BlogComment" do
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-    permit_params :title, :body, :status, :user_id, :discarded_at
+  # permit_params :text, :blog_id, :user_id, :discarded_at
   #
   # or
   #
   # permit_params do
-  #   permitted = [:title, :body, :status, :user_id, :discarded_at]
+  #   permitted = [:text, :blog_id, :user_id, :discarded_at]
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
