afd1f263115e5e7e9c19e2377f53e0ac610c1249
zain <iamzayn19@gmail.com>
2022-08-16 12:25:06 +0500

    Add BlogLike model to Active Admin

---
+++ app/admin/blog_likes.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register BlogLike do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :user_id, :blog_id, :discarded_at
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:user_id, :blog_id, :discarded_at]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
