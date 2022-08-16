3476cfd79edff8d15e240bdf1fb49b169e9f12cf
zain <iamzayn19@gmail.com>
2022-08-16 11:36:07 +0500

    Add active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   #root 'api/v1/blogs#index'
 
   devise_for :admin , controllers: {
