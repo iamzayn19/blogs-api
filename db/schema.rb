35443f28d79d972353e1708713097f8739d8a0e7
zain <iamzayn19@gmail.com>
2022-08-16 15:19:36 +0500

    Change status column in Blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_16_101520) do
+ActiveRecord::Schema.define(version: 2022_08_16_101816) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 2022_08_16_101520) do
   create_table "blogs", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
-    t.integer "status"
+    t.integer "status", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
