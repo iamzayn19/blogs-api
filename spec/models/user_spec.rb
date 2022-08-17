bf974182075d2309fc38e6740d751651c59977c8
zain <iamzayn19@gmail.com>
2022-08-17 11:51:48 +0500

    Write basic validation tests for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,25 @@ RSpec.describe User, type: :model do
     it "is valid with valid attributes" do
       expect(subject).to be_valid
     end
+
+    it "is not valid without a first_name" do
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a last_name" do
+      subject.last_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without an email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a password" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
   end 
 end
