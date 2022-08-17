b6aa3ba0c51744e3930eed9810da0be7ee09ef21
zain <iamzayn19@gmail.com>
2022-08-17 12:59:03 +0500

    Add foreign key to comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,12 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   
-
-  describe "Associations" do
-    it { should belong_to(:user)}
-    it { should belong_to(:blog) }
-  end
-
   describe "Validations" do
     subject { described_class.new(text: "Anything", user_id: 3, blog_id: 13) }
     
@@ -20,4 +14,9 @@ RSpec.describe Comment, type: :model do
       expect(subject).to be_valid
     end
   end 
+
+  describe "Associations" do
+    it { should belong_to(:user)}
+    it { should belong_to(:blog) }
+  end
 end
