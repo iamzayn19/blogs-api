0ee3baa283cc2742765fa607d5a0c30d79b9b423
zain <iamzayn19@gmail.com>
2022-08-18 16:16:56 +0500

    Add blog via factory bot

--- spec/requests/blogs_spec.rb
+++ spec/requests/blogs_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 RSpec.describe "Blogs", type: :request do
   user = FactoryBot.create(:user)
-  
+  blog = FactoryBot.create(:blog)
   let(:auth_params) { {email: user.email, password: user.password } }
+  let(:blog_params) { { title: blog.title, body: blog.body, user_id: user.id} }
   let(:token) { { Authorization: "" }}
   describe "testing blogs" do
 
@@ -20,14 +21,14 @@ RSpec.describe "Blogs", type: :request do
     it "created a blog successfully" do
       user.user_type = 0
       user.save
-      post '/api/v1/blogs', params:  { title: 'sample title' , body: 'sample body', user_id: '1' }, headers: token
+      post '/api/v1/blogs', params: blog_params, headers: token
       expect(response).to have_http_status(200)
       puts JSON.parse(response.body)
     end
 
 
     it "unauthorized user cannot create a blog" do
-      post '/api/v1/blogs', params:  { title: 'sample title' , body: 'sample body', user_id: '1' }
+      post '/api/v1/blogs', params: blog_params
       expect(response).to have_http_status(200)
       puts JSON.parse(response.body)
     end
@@ -35,7 +36,7 @@ RSpec.describe "Blogs", type: :request do
     it "user cannot create a blog if he/she is a public user" do
       user.user_type = 1
       user.save
-      post '/api/v1/blogs', params:  { title: 'sample title' , body: 'sample body', user_id: '1' }, headers: token
+      post '/api/v1/blogs', params: blog_params, headers: token
       expect(response).to have_http_status(401)
       puts JSON.parse(response.body)
     end
