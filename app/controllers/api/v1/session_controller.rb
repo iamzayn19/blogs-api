e4f85911bd2e5819e8a4dc58e3fea2f51206732e
zain <iamzayn19@gmail.com>
2022-08-19 19:38:07 +0500

    Add logic to block unconfirmed account login

--- app/controllers/api/v1/session_controller.rb
+++ app/controllers/api/v1/session_controller.rb
@@ -4,8 +4,12 @@ class Api::V1::SessionController < Api::V1::ApiController
   def create
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
-      token = jwt_encode(user_id: @user.id)
-      render json: { token: token }, status: :ok
+      if @user.confirmed?
+        token = jwt_encode(user_id: @user.id)
+        render json: { token: token }, status: :ok
+      else  
+        render json: { error: "Please confirm your account first, then attempt login. "}, status: :unauthorized
+      end
     else 
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
