3d97a4235010afbf08dea23eb453748e463b9c60
zain <iamzayn19@gmail.com>
2022-08-22 01:52:45 +0500

    Add comfirmation email feature

--- app/controllers/api/v1/confirmation_controller.rb
+++ app/controllers/api/v1/confirmation_controller.rb
@@ -1,11 +1,21 @@
 class Api::V1::ConfirmationController < Api::V1::ApiController
+  include JsonWebToken
   skip_before_action :authenticate_request
-  
+
   def create
-    user = User.find(params[:id])
-    user.confirmed = true
-    user.save
-    render json: "Your account has been confirmed successfully!"
+    @token = params["token"]
+    decoded = jwt_decode(@token)
+    if decoded.empty?
+      user = User.find(params[:id])
+      token = jwt_confirmation_encode(user_id: user.id)
+      UserMailer.resend_confirmation(user,token).deliver
+      render json: "Your token has expired. Please wait for the new confirmation token."
+    else 
+      @current_user = User.find(decoded["user_id"])
+      @current_user.confirmed = true
+      @current_user.save
+      render json: "Your account has been confirmed successfully!"
+    end
   end
 
 end
--- app/controllers/api/v1/session_controller.rb
+++ app/controllers/api/v1/session_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::SessionController < Api::V1::ApiController
     @user = User.find_by_email(params[:email])
     if @user&.authenticate(params[:password])
       if @user.confirmed?
-        token = jwt_encode(user_id: @user.id)
+        token = jwt_session_encode(user_id: @user.id)
         render json: { token: token }, status: :ok
       else  
         render json: { error: "Please confirm your account first, then attempt login. "}, status: :unauthorized
