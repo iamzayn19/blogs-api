e9ce978a95d1c14d1ddf124a9978010051888f56
zain <iamzayn19@gmail.com>
2022-08-22 02:36:20 +0500

    Modify blog_likes ref from blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_19_142938) do
+ActiveRecord::Schema.define(version: 2022_08_21_213420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,7 +131,7 @@ ActiveRecord::Schema.define(version: 2022_08_19_142938) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "blog_likes", "blogs"
+  add_foreign_key "blog_likes", "blogs", on_delete: :cascade
   add_foreign_key "blog_likes", "users"
   add_foreign_key "blogs", "users"
   add_foreign_key "comments", "blogs"
